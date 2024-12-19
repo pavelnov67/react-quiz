672cffabafbe88ff2c9787345579dc67685e72b4
pavelnov67 <passatb567@ya.ru>
2024-12-19 19:37:57 +0300

    upd styles

--- src/components/UI/adminPage/Menu.js
+++ src/components/UI/adminPage/Menu.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
 import styles from './adminPage.module.css'
 
-const Menu = (props) => {
+const Menu = () => {
   return (
     <nav className={styles.home_anchors}>
       <Link to="add_question_page">Добавление вопроса</Link>
+      <hr className={styles.mainLayoutHR} />
       <Link to="quiz">Игра</Link>
     </nav>
   )
