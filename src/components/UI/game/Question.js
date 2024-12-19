acd381e9bb221b778bcdb806d79e894abeb8a20e
pavelnov67 <passatb567@ya.ru>
2024-12-19 12:32:18 +0300

    ref: question page styles
    сделал базовую вёрстку страницы с вопросами и ответами

--- src/components/UI/game/Question.js
+++ src/components/UI/game/Question.js
@@ -1,19 +1,23 @@
-import styles from '../ui.module.css'
+import styles from './question.module.css'
 import data from '../../data/data.json'
 
 const Question = (props) => {
   const questions = data.data.questions
 
   return (
-    <>
+    <main className={styles.questions_container}>
       {questions.map((question) => (
         <div
           key={question.id}
           className={styles.question_container}
         >
-          <h2>{question.id}</h2>
+          <h2>Вопрос №{question.id}</h2>
           <h3>{question.title}</h3>
           <hr></hr>
+          <div className={styles.answer_header}>
+            <p>Ответы</p>
+            <p>Баллы</p>
+          </div>
           <div className={styles.answer_container}>
             {question.answers.map((answer) => (
               <div
@@ -27,7 +31,7 @@ const Question = (props) => {
           </div>
         </div>
       ))}
-    </>
+    </main>
   )
 }
 
