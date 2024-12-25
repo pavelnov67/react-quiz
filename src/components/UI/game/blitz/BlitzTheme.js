7cf5b294aa2aa3dc8a00a874781443a1b3578a62
pavelnov67 <passatb567@ya.ru>
2024-12-25 21:36:41 +0300

    debug: get question data

--- src/components/UI/game/blitz/BlitzTheme.js
+++ src/components/UI/game/blitz/BlitzTheme.js
@@ -8,7 +8,9 @@ const BlitzTheme = ({ id, description, title }) => {
   console.log(id)
   console.log(description)
   console.log(title)
-  const [questionsData, setQuestionsData] = useState({})
+  const [questionsData, setQuestionsData] = useState([
+    { description: 'test1', id: 1, title: 'test1' },
+  ])
 
   useEffect(() => {
     const handleClickQuestions = async () => {
@@ -16,7 +18,7 @@ const BlitzTheme = ({ id, description, title }) => {
         const data = await axios.get(
           `${base_URL}/game/blitz.questions_list?theme_id=1`
         )
-        setQuestionsData(data)
+        setQuestionsData(data.data.data.questions)
       } catch (e) {
         console.log(e)
       }
@@ -24,7 +26,19 @@ const BlitzTheme = ({ id, description, title }) => {
     handleClickQuestions()
   }, [])
 
-  return <BlitzQuestion />
+  const questions = questionsData.map((question) => question.title)
+  console.log(questions)
+
+  return (
+    <>
+      {questionsData.map((question) => (
+        <div key={question.id}>
+          <h3>{question.title}</h3>
+          <h3>{question.answer}</h3>
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default BlitzTheme
