74e8b3ae5d1472f7a01a02b6feda07b1a785ee3b
pavelnov67 <passatb567@ya.ru>
2024-12-25 18:48:32 +0300

    add: add theme
    реализовал форму успешного добавления темы

--- src/components/UI/game/blitz/BlitzAddTheme.js
+++ src/components/UI/game/blitz/BlitzAddTheme.js
@@ -1,13 +1,51 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { useFormik } from 'formik'
+import { base_URL } from '../../../variables/vars'
 
 const BlitzAddTheme = () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+    },
+    onSubmit: (values) => {
+      const instance = axios.create({
+        timeout: 5000,
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      try {
+        instance.post(`${base_URL}/game/blitz.themes_add`, values)
+      } catch (err) {
+        console.log(err.message)
+      }
+    },
+  })
+
   return (
-    <form>
-      <label>title</label>
-      <input />
-      <label>description</label>
-      <input />
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="title">Название темы</label>
+      <input
+        id="title"
+        name="title"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.title}
+      />
+      <label htmlFor="description">Описание темы</label>
+      <input
+        id="description"
+        name="description"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.description}
+      />
+      <button type="submit">Сохранить</button>
     </form>
   )
 }
