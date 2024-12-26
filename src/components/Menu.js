8f704ce327f1a921f92c212045b0a611a7d795ce
pavelnov67 <passatb567@ya.ru>
2024-12-26 16:24:17 +0300

    add: btn login to user Email

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,48 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+import { base_URL } from './variables/vars'
 import styles from './UI/adminPage/adminPage.module.css'
 
 const Menu = () => {
+  const [isLogged, setIsLogged] = useState(false)
+  const [userName, setUserName] = useState('')
   const navigate = useNavigate()
+
   const handleAuth = () => {
     navigate('/auth')
   }
+
+  const handleClickThemes = async () => {
+    try {
+      const userData = await axios.get(`${base_URL}/admin.current`)
+      setUserName(userData.data.data.email)
+      setIsLogged(true)
+    } catch (err) {
+      console.log(err)
+      toast.error(err.message)
+    }
+  }
+
   return (
     <div className={styles.menu_container}>
+      <ToastContainer position="bottom-right" autoClose={2000} />
       <header>
         <h2>Страница администратора</h2>
-        <button className={styles.login_btn} type="button" onClick={handleAuth}>
-          Войти
-        </button>
+        {isLogged ? (
+          <h4>{userName}</h4>
+        ) : (
+          <button
+            className={styles.login_btn}
+            type="button"
+            onClick={handleClickThemes}
+          >
+            Войти
+          </button>
+        )}
       </header>
       <div className={styles.side_container}>
         <div className={styles.sidebar}>
