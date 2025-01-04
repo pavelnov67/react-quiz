d197625a9a82d4d47aac3129309e035658bdb4ee
pavelnov67 <passatb567@ya.ru>
2025-01-04 10:32:23 +0300

    ref: auth validation

--- src/components/authorization/Auth.js
+++ src/components/authorization/Auth.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify'
 import styles from './auth.module.css'
 import { base_URL } from '../variables/vars'
 
@@ -16,41 +15,41 @@ const Auth = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const instance = axios.create({
-      baseURL: url_APIPost,
-      timeout: 5000,
-      headers: {
-        accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-    try {
-      const loginBody = {
-        email,
-        password,
+    if (email && password) {
+      const instance = axios.create({
+        baseURL: url_APIPost,
+        timeout: 5000,
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      try {
+        const loginBody = {
+          email,
+          password,
+        }
+        await instance.post(url_APIPost, loginBody)
+        navigate('/')
+      } catch (err) {
+        if (err.status === 403) {
+          setError('Неверные логин или пароль')
+        }
       }
-      await instance.post(url_APIPost, loginBody)
-      //navigate('/')
-    } catch (err) {
-      if (err.status === 403) {
-        setError('Неверные логин или пароль')
-      }
+    } else {
+      setError('Все поля должны быть заполнены')
     }
   }
 
   return (
     <div className={styles.authFormContainer}>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={2000}
-      />
       <form
         className={styles.authForm}
         onSubmit={handleSubmit}
       >
         <h1>Авторизация</h1>
         <div className={styles.form_inputs}>
-          <label>Email</label>
+          <label>Электронная почта</label>
           <input
             placeholder="Введите email"
             onChange={(e) => setEmail(e.target.value)}
